e0f65d0250a7a737f0a361f1ba70fa124ecf680d
qiao <whitekinbar@outlook.com>
2023-04-05 23:22:32 -0400

    Spark2

--- Spark Assignment 2/src/main/scala/titanic.scala
+++ Spark Assignment 2/src/main/scala/titanic.scala
@@ -5,26 +5,30 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.SparkConf
 
 
+
 object titanic {
   def modify(data: DataFrame): DataFrame = {
+
     val meanValueAge = data.select(mean("Age")).first()(0).asInstanceOf[Double]
-    val meanValueFare = data.select(mean("Age")).first()(0).asInstanceOf[Double]
+    val meanValueFare = data.select(mean("Fare")).first()(0).asInstanceOf[Double]
+
     val dfFilled0 = data.na.fill(meanValueAge, Seq("Age"))
     val dfFilled = dfFilled0.na.fill(meanValueFare, Seq("Fare"))
+
     val dfModified = dfFilled
       .withColumn("Sex", when(col("Sex") === "female", 1).otherwise(0))
       .withColumn("Embarked1", when(col("Embarked") === "S", 1).otherwise(0))
       .withColumn("Embarked2", when(col("Embarked") === "C", 1).otherwise(0))
       .withColumn("Embarked3", when(col("Embarked") === "Q", 1).otherwise(0))
 
-
     val assembler = new VectorAssembler()
       .setInputCols(Array("Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked1", "Embarked2", "Embarked3"))
       .setOutputCol("features")
-    val idk = assembler.transform(dfModified)
-    val preparedData = idk.select("features", "Survived")
-    preparedData.show()
-    preparedData
+
+    val modify_data = assembler.transform(dfModified)
+
+    modify_data
+
   }
   def main(args: Array[String]): Unit = {
 
@@ -51,40 +55,20 @@ object titanic {
       .csv("/Users/junlongqiao/data/test.csv")
       .limit(419)
 
-    val testlabel = spark.read
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .csv("/Users/junlongqiao/data/gender_submission.csv")
-      .limit(419)
-
-    val testdata = testfeatures.join(testlabel,"PassengerId")
-
-    val train = modify(traindata)
-    val test = modify(testdata)
+    val train = modify(traindata).select("features", "Survived")
+    val test = modify(testfeatures).select("features")
 
     val dt = new DecisionTreeClassifier().setLabelCol("Survived").setFeaturesCol("features")
-    val rf = new RandomForestClassifier().setLabelCol("Survived").setFeaturesCol("features")
-    val gbt = new GBTClassifier().setLabelCol("Survived").setFeaturesCol("features")
 
     val dtModel = dt.fit(train)
-    val rfModel = rf.fit(train)
-    val gbtModel = gbt.fit(train)
 
     val dtPredictions = dtModel.transform(test)
-    val rfPredictions = rfModel.transform(test)
-    val gbtPredictions = gbtModel.transform(test)
 
-    val dtA = dtPredictions.withColumn("Accuracy", when(col("Survived") === col("prediction"), 1).otherwise(0))
-    val rfA = rfPredictions.withColumn("Accuracy", when(col("Survived") === col("prediction"), 1).otherwise(0))
-    val gbtA = gbtPredictions.withColumn("Accuracy", when(col("Survived") === col("prediction"), 1).otherwise(0))
+    val dtcsv = dtPredictions.withColumn("PassengerId", monotonically_increasing_id() + 892)
 
-    val dtAccuracy = dtA.select(mean("Accuracy")).first.getDouble(0)*100
-    val rfAccuracy = rfA.select(mean("Accuracy")).first.getDouble(0)*100
-    val gbtAccuracy = gbtA.select(mean("Accuracy")).first.getDouble(0)*100
+    val dtCSV = dtcsv.withColumnRenamed("prediction", "Survived").select("PassengerId","Survived")
 
-    println(f"$dtAccuracy%.2f%%")
-    println(f"$rfAccuracy%.2f%%")
-    println(f"$gbtAccuracy%.2f%%")
+    dtCSV.write.option("header", "true").csv("/Users/junlongqiao/data/dt")
 
   }
 }
